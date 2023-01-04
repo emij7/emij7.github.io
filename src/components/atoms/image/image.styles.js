d94d1fcfe307c1c869353cdf950fab92359f7128
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-01-03 21:10:49 -0300

    add: resume image type

--- src/components/atoms/image/image.styles.js
+++ src/components/atoms/image/image.styles.js
@@ -3,8 +3,14 @@ import styled, { css } from "styled-components";
 export const StyledImage = styled.img`
   height: ${(props) => props.size};
   ${({ secondary }) => secondary && projectImg}
+  ${({ resumeImage }) => resumeImage && personalImage}
 `;
 const projectImg = css`
   width: 100%;
   padding: 0.5rem;
+`;
+const personalImage = css`
+  filter: ${({ type, theme }) =>
+    type === "primary" ? theme.filter : theme.filter};
+  border-radius: 20%;
 `;
