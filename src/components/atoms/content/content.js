c6bcaa5fcae4da59b00d9d566b04adce270977ee
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-01-04 21:09:18 -0300

    feat: content now accept margin

--- src/components/atoms/content/content.js
+++ src/components/atoms/content/content.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { StyledContent } from "./content.styles";
 
-export const Content = ({ children, height = "auto", width = "auto" }) => {
+export const Content = ({
+  children,
+  height = "auto",
+  width = "auto",
+  margin = "0 0",
+}) => {
   return (
-    <StyledContent height={height} width={width}>
+    <StyledContent height={height} width={width} margin={margin}>
       {children}
     </StyledContent>
   );
--- src/components/atoms/content/content.styles.js
+++ src/components/atoms/content/content.styles.js
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const StyledContent = styled.div`
   height: ${(props) => props.height};
   width: ${(props) => props.width};
+  margin: ${(props) => props.margin};
 `;
