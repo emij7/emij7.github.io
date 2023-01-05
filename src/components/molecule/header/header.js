51bbf8acefa462caf422a6f8e10205d7b416b68b
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-01-05 09:12:52 -0300

    feat: animated title

--- src/components/molecule/header/header.js
+++ src/components/molecule/header/header.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { text } from "../../../text/text";
-import { StyledTitle } from "../../atoms/title/title.styles";
+import { StyledAnimatedTitle } from "../../atoms/title/title.styles";
 import { StyledDecoration, StyledHeader } from "./header.styles";
 
 export const Header = ({ language }) => {
   return (
     <React.Fragment>
       <StyledHeader>
-        <StyledTitle type="primary">Emiliano Juarez</StyledTitle>
+        <StyledAnimatedTitle type="primary">
+          Emiliano Juarez
+        </StyledAnimatedTitle>
         <StyledDecoration />
-        <StyledTitle type="secondary">{text.title[language]}</StyledTitle>
+        <StyledAnimatedTitle type="secondary">
+          {text.title[language]}
+        </StyledAnimatedTitle>
       </StyledHeader>
     </React.Fragment>
   );
