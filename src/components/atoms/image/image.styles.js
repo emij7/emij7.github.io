335051278ff83a31360a5fc3f922ff743f8cb16b
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-01-05 09:12:32 -0300

    fix: commented code removed

--- src/components/atoms/image/image.styles.js
+++ src/components/atoms/image/image.styles.js
@@ -13,13 +13,10 @@ const personalImage = css`
   filter: ${({ type, theme }) =>
     type === "primary" ? theme.filter : theme.filter};
   border-radius: 20%;
-  /* @media (${({ theme }) => theme.device.tablet}) {
-    height: ${(props) => props.size / 2};
-  } ; */
   @media (${({ theme }) => theme.device.tablet}) {
     height: ${(props) => props.size / 2 + "rem"};
   }
   @media (${({ theme }) => theme.device.desktop}) {
     height: ${(props) => props.size / 1.5 + "rem"};
-  } ;
+  }
 `;
