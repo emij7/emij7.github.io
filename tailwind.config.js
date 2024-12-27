4884acae4a07183084ca149a313dc037a004f79d
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2024-12-26 21:26:10 -0300

    Tailwind config and home styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,21 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+      },
+      colors: {
+        background: "rgb(var(--background))",
+        foreground: "rgb(var(--foreground))",
+        muted: "rgb(var(--muted))",
+        accent: "rgb(var(--accent))",
+      },
+    },
   },
   plugins: [],
 };
