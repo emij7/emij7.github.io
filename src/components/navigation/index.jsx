ccbe79d09d3b893c89f47ba2bf7c2a4cee904c33
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2025-01-09 22:40:01 -0300

    Icons position sorrounding logo

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -6,24 +6,27 @@ const Navigation = () => {
   const angleIncrement = 360 / BtnList.length;
 
   return (
-    <div className="flex items-center justify-between relative ">
-      {BtnList.map((btn, index) => {
-        const angle = (angleIncrement * Math.PI) / 180;
-        const radius = "calc(20vw - 1rem)";
-        const x = `calc(${radius} * Math.cos(${angle})`;
-        const y = `calc(${radius} * Math.sin(${angle})`;
-        return (
-          <button
-            key={index}
-            // className="absolute"
-            style={{
-              transform: `translate(${x}, ${y})`,
-            }}
-          >
-            {btn.label}
-          </button>
-        );
-      })}
+    <div class="w-full fixed h-screen flex items-center justify-center">
+      <div className="flex items-center justify-between relative">
+        {BtnList.map((btn, index) => {
+          const angle = (index * angleIncrement * Math.PI) / 180;
+          const radius = "calc(22vw - 1rem)";
+          const x = `calc(${radius} * ${Math.cos(angle)})`;
+          const y = `calc(${radius} * ${Math.sin(angle)})`;
+          console.log(index, angle, radius, x, y);
+          return (
+            <button
+              key={index}
+              className="absolute"
+              style={{
+                transform: `translate(${x}, ${y})`,
+              }}
+            >
+              {btn.label}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
