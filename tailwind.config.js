a23085357d644813a90c3728f3c12e3b7473d30f
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2025-01-09 22:05:22 -0300

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./**/*.{html,js,jsx,ts,tsx}", "!./node_modules/**/*"],
   theme: {
     extend: {
       fontFamily: {
