8106affd7184d1443884cfc3ceba4805c8d8d50a
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2025-02-05 17:44:30 -0300

    Fix: mobile padding fixed

--- src/app/(sub pages)/layout.js
+++ src/app/(sub pages)/layout.js
@@ -2,7 +2,7 @@ import HomeButton from "../../components/HomeButton";
 
 export default function SubPagesLayout({ children }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center py-20 px-20 ">
+    <main className="flex min-h-screen flex-col items-center justify-center py-20 md:px-20 px-8">
       <HomeButton />
       {children}
     </main>
